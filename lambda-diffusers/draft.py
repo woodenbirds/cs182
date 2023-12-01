41ea8eeb836aee32ee7bc03415271bea3a008bdf
xiaochy <xiaochy@shanghaitech.edu.cn>
2023-12-01 14:19:28 -0800

    change the structure of github repo

--- lambda-diffusers/draft.py
+++
@@ -1,9 +0,0 @@
-import csv
-csv_reader=csv.reader(open("full_emoji.csv"))
-j=0
-for row in csv_reader:
-    for i in range(len(row)):
-        print(i,row[i])
-    j+=1
-    if(j==2):
-        break
